d040f08f5515d53a67aac853d28b62f6abc67227
Andineitor <andiloor2809@gmail.com>
2024-07-19 00:31:08 -0500

    CRUD Rol finalizado

--- Pandulzura_WinPresentacion/FormRol.Designer.cs
+++ Pandulzura_WinPresentacion/FormRol.Designer.cs
@@ -38,6 +38,8 @@
             this.btnLimpiar = new System.Windows.Forms.Button();
             this.groupBox1 = new System.Windows.Forms.GroupBox();
             this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnBuscar = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.dgvRol)).BeginInit();
             this.groupBox1.SuspendLayout();
             this.SuspendLayout();
@@ -46,42 +48,44 @@
             // 
             this.label1.AutoSize = true;
             this.label1.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label1.Location = new System.Drawing.Point(31, 44);
+            this.label1.Location = new System.Drawing.Point(57, 228);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 20);
+            this.label1.Size = new System.Drawing.Size(55, 20);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Rol";
+            this.label1.Text = "Rol Id:";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label2
             // 
             this.label2.AutoSize = true;
             this.label2.ForeColor = System.Drawing.SystemColors.Highlight;
-            this.label2.Location = new System.Drawing.Point(31, 106);
+            this.label2.Location = new System.Drawing.Point(85, 51);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(98, 20);
             this.label2.TabIndex = 1;
             this.label2.Text = "Nombre_Rol";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
             // 
             // txtRol
             // 
-            this.txtRol.Location = new System.Drawing.Point(168, 38);
+            this.txtRol.Location = new System.Drawing.Point(114, 225);
             this.txtRol.Name = "txtRol";
-            this.txtRol.Size = new System.Drawing.Size(216, 26);
+            this.txtRol.Size = new System.Drawing.Size(76, 26);
             this.txtRol.TabIndex = 2;
             this.txtRol.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // txtNombre
             // 
-            this.txtNombre.Location = new System.Drawing.Point(168, 100);
+            this.txtNombre.Location = new System.Drawing.Point(212, 51);
             this.txtNombre.Name = "txtNombre";
             this.txtNombre.Size = new System.Drawing.Size(216, 26);
             this.txtNombre.TabIndex = 3;
+            this.txtNombre.TextChanged += new System.EventHandler(this.txtNombre_TextChanged);
             // 
             // btnAgregar
             // 
             this.btnAgregar.BackColor = System.Drawing.SystemColors.Highlight;
-            this.btnAgregar.Location = new System.Drawing.Point(483, 100);
+            this.btnAgregar.Location = new System.Drawing.Point(182, 105);
             this.btnAgregar.Name = "btnAgregar";
             this.btnAgregar.Size = new System.Drawing.Size(101, 38);
             this.btnAgregar.TabIndex = 4;
@@ -92,7 +96,7 @@
             // btnListar
             // 
             this.btnListar.BackColor = System.Drawing.SystemColors.MenuHighlight;
-            this.btnListar.Location = new System.Drawing.Point(333, 189);
+            this.btnListar.Location = new System.Drawing.Point(373, 105);
             this.btnListar.Name = "btnListar";
             this.btnListar.Size = new System.Drawing.Size(101, 40);
             this.btnListar.TabIndex = 5;
@@ -103,18 +107,18 @@
             // dgvRol
             // 
             this.dgvRol.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvRol.Location = new System.Drawing.Point(49, 244);
+            this.dgvRol.Location = new System.Drawing.Point(272, 213);
             this.dgvRol.Name = "dgvRol";
             this.dgvRol.RowHeadersWidth = 62;
             this.dgvRol.RowTemplate.Height = 28;
-            this.dgvRol.Size = new System.Drawing.Size(628, 155);
+            this.dgvRol.Size = new System.Drawing.Size(368, 171);
             this.dgvRol.TabIndex = 6;
             // 
             // btnLimpiar
             // 
             this.btnLimpiar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
             this.btnLimpiar.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnLimpiar.Location = new System.Drawing.Point(483, 25);
+            this.btnLimpiar.Location = new System.Drawing.Point(509, 44);
             this.btnLimpiar.Name = "btnLimpiar";
             this.btnLimpiar.Size = new System.Drawing.Size(101, 40);
             this.btnLimpiar.TabIndex = 7;
@@ -126,10 +130,9 @@
             // 
             this.groupBox1.Controls.Add(this.btnLimpiar);
             this.groupBox1.Controls.Add(this.txtNombre);
-            this.groupBox1.Controls.Add(this.txtRol);
             this.groupBox1.Controls.Add(this.btnAgregar);
+            this.groupBox1.Controls.Add(this.btnListar);
             this.groupBox1.Controls.Add(this.label2);
-            this.groupBox1.Controls.Add(this.label1);
             this.groupBox1.Location = new System.Drawing.Point(60, 12);
             this.groupBox1.Name = "groupBox1";
             this.groupBox1.Size = new System.Drawing.Size(628, 160);
@@ -137,14 +140,39 @@
             this.groupBox1.TabStop = false;
             this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
             // 
+            // btnEliminar
+            // 
+            this.btnEliminar.BackColor = System.Drawing.Color.Red;
+            this.btnEliminar.Location = new System.Drawing.Point(114, 347);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 37);
+            this.btnEliminar.TabIndex = 9;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = false;
+            this.btnEliminar.Click += new System.EventHandler(this.btnEliminar_Click);
+            // 
+            // btnBuscar
+            // 
+            this.btnBuscar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(255)))), ((int)(((byte)(128)))));
+            this.btnBuscar.Location = new System.Drawing.Point(115, 287);
+            this.btnBuscar.Name = "btnBuscar";
+            this.btnBuscar.Size = new System.Drawing.Size(75, 35);
+            this.btnBuscar.TabIndex = 10;
+            this.btnBuscar.Text = "Buscar";
+            this.btnBuscar.UseVisualStyleBackColor = false;
+            this.btnBuscar.Click += new System.EventHandler(this.btnBuscar_Click);
+            // 
             // FormRol
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(724, 427);
+            this.Controls.Add(this.btnBuscar);
+            this.Controls.Add(this.btnEliminar);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.dgvRol);
-            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.txtRol);
+            this.Controls.Add(this.label1);
             this.Name = "FormRol";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "FormRol";
@@ -153,6 +181,7 @@
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -168,5 +197,7 @@
         private System.Windows.Forms.Button btnLimpiar;
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.ColorDialog colorDialog1;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnBuscar;
     }
 }
